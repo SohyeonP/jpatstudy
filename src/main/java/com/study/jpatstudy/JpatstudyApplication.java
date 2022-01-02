6a8d97c6077577eb8a4b77fa69c227eb9eed225c
SohyeonPark <hidhen9714@gmail.com>
2022-01-02 22:39:41 +0900

    commit

---
+++ src/main/java/com/study/jpatstudy/JpatstudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.jpatstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpatstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpatstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/jpatstudy/JpatstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jpatstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpatstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
